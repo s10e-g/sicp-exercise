db2318f6e05b29a1e9080c02293e74d64d9008b1
shadowmourne <s10e@live.com>
2013-02-22 00:42:12 +0800

    ex-2.47

---
+++ Chapter-2/47.scm
@@ -0,0 +1,19 @@
+;;; ex-2.47
+(define (make-frame1 origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define origin-frame1 car)
+
+(define edge1-frame1 cadr)
+
+(define edge2-frame1 caddr)
+
+
+(define (make-frame2 origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define origin-frame2 car)
+
+(define edge1-frame2 cadr)
+
+(define edge2-frame2 cddr)
