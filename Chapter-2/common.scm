c614620bcb95eeb0d5afdd99b12cc2479a86621b
shadowmourne <s10e@live.com>
2013-02-22 03:07:29 +0800

    update 2.2.4

--- Chapter-2/common.scm
+++ Chapter-2/common.scm
@@ -19,3 +19,23 @@
 
 (define (flatmap proc seq)
   (accumulate append '() (map proc seq)))
+
+
+;;; 2.2.4
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+      (origin-frame frame)
+      (add-vect (scale-vect (xcor-vect v)
+                            (edge1-frame frame))
+                (scale-vect (ycor-vect v)
+                            (edge2-frame frame))))))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+      (lambda (segment)
+        (draw-line
+          ((frame-coord-map frame) (start-segment segment))
+          ((frame-coord-map frame) (end-segment segment))))
+      segment-list)))
