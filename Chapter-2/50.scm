7e763af94b78fa3460384c6587f518c05693d585
shadowmourne <s10e@live.com>
2013-02-23 03:11:01 +0800

    ex-2.50

---
+++ Chapter-2/50.scm
@@ -0,0 +1,19 @@
+;;; ex-2.50
+(load "common.scm")
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.0 1.0)))
+
+(define (minus180d painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (minus270d painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
