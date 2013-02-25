af99b7e6a098c57c9a67fc4b9e6d178056462dfd
shadowmourne <s10e@live.com>
2013-02-25 21:02:32 +0800

    ex-2.59

---
+++ Chapter-2/59.scm
@@ -0,0 +1,9 @@
+;;; ex-2.59
+(load "common.scm")
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1) (cons (car set1) set2)))))
