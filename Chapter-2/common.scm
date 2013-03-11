a089f2e370e4ac29e377921920528f20de2df7a4
shadowmourne <s10e@live.com>
2013-03-11 22:08:01 +0800

    update attach-tag...

--- Chapter-2/common.scm
+++ Chapter-2/common.scm
@@ -251,3 +251,17 @@
       (adjoin-leaf-set (make-leaf (car pair)
                              (cadr pair))
                   (make-leaf-set (cdr pairs))))))
+
+;;; 2.4.2
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- CONTENTS" datum)))
