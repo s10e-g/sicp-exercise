90eadd9cb191f299d979604d1f7990c15f80aa09
shadowmourne <s10e@live.com>
2013-03-13 22:56:43 +0800

    ex-2.79

---
+++ Chapter-2/79.scm
@@ -0,0 +1,21 @@
+;;; ex-2.79
+(load "common.scm")
+
+(define (equ? x y)
+  (apply-generic 'equ? x y))
+
+(define (install-equ-package)
+  (put 'equ? '(scheme-number scheme-number)
+       (lambda (x y)
+         (= x y)))
+  (put 'equ? '(rational rational)
+       (lambda (x y)
+         (and (= (numer x) (numer y))
+              (= (denom x) (denom y)))))
+  (put 'equ? '(complex complex)
+       (lambda (x y)
+         (or (and (= (real-part x) (real-part y))
+                  (= (imag-part x) (imag-part y)))
+             (and (= (magnitude x) (magnitude y))
+                  (= (angle x) (angle y))))))
+  'done)
